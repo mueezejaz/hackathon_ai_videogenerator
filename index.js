7cee1c75b666b0c1ed1688970c5f4cb46c2e8674
mueez <mueezejaz12345678@gmail.com>
2025-10-28 12:47:44 +0500

    connect front end with backend to to serve build with http server

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("server is running");
-});
+app.use(express.static(path.join(__dirname, './client/dist')))
+
+// app.get("/", (req, res) => {
+//   res.send("server is running");
+// });
+
 
 app.listen(PORT, () => {
   console.log(` Server is running at http://localhost:${PORT}`);
