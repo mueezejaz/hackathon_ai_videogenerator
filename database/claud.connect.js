fdac80b7c614d1854015fd1bf1c8336588d05ed1
mueez <mueezejaz12345678@gmail.com>
2025-10-28 23:51:42 +0500

    set up claudanry to upload video

---
+++ database/claud.connect.js
@@ -0,0 +1,13 @@
+import { v2 as cloudinary } from "cloudinary";
+import { config } from '../config/env.js'
+
+
+cloudinary.config({
+  cloud_name: config.get(cloud_name),
+  api_key: config.get(api_key),
+  api_secret: config.get(api_secret),
+});
+
+export default cloudinary;
+
+
